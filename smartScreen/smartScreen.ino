b75f77cf58fda4e53019c71653e97da38c1efd01
B1naryB0t <47212121+MahdiTheGreat@users.noreply.github.com>
2022-03-23 16:38:05 +0430

    Initial commit

---
+++ smartScreen/smartScreen.ino
@@ -0,0 +1,12 @@
+const int ledOutput = A1;// Initialize Pin11 for connecting LED
+const int ldrInput = A0;// Initialize PIN A0 LDR PIN
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledOutput, OUTPUT);// Define LED pin as output
+  pinMode(ldrInput, INPUT);// Define LDR pin as input 
+}
+void loop() {
+
+  int ldrStatus = analogRead(ldrInput); // read LDR lignt intensity as analog value
+  analogWrite(ledOutput,ldrStatus);
+}
